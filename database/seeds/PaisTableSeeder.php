3058f9ecd2de3b4e37ba5718f06c9a7728df5545
Wouerner Brandão <wouerner@protonmail.com>
2018-04-18 19:46:51 -0300

    :love_letter: [UPDATE] Novas configurações do docker.

--- database/seeds/PaisTableSeeder.php
+++ database/seeds/PaisTableSeeder.php
@@ -12,7 +12,7 @@ class PaisTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('pais')->insert([
+        $id = DB::table('pais')->insert([
             'nome' => 'Brasil',
         ]);
 
@@ -20,24 +20,82 @@ class PaisTableSeeder extends Seeder
             'nome' => 'Argentina',
         ]);
 
+        $p = DB::select('select * from pais');
+
         DB::table('regioes')->insert([
-            'pais_id' => 1,
+            'pais_id' => $p[0]->id,
             'nome' => 'Norte',
         ]);
 
         DB::table('regioes')->insert([
-            'pais_id' => 1,
+            'pais_id' => $p[0]->id,
             'nome' => 'Nordeste',
         ]);
 
+        DB::table('regioes')->insert([
+            'pais_id' => $p[0]->id,
+            'nome' => 'Centro-Oeste',
+        ]);
+
+        DB::table('regioes')->insert([
+            'pais_id' => $p[0]->id,
+            'nome' => 'Sudeste',
+        ]);
+
+        DB::table('regioes')->insert([
+            'pais_id' => $p[0]->id,
+            'nome' => 'Sul',
+        ]);
+
+		// Região Norte
         DB::table('comidas')->insert([
             'regiao_id' => 1,
-            'nome' => 'acarajé',
+            'nome' => 'tucupi',
         ]);
 
         DB::table('comidas')->insert([
             'regiao_id' => 1,
-            'nome' => 'cuscuz',
+            'nome' => 'açaí',
         ]);
+
+		// Região Nordeste
+        DB::table('comidas')->insert([
+            'regiao_id' => 2,
+            'nome' => 'mugunzá',
+        ]);
+
+        DB::table('comidas')->insert([
+            'regiao_id' => 2,
+            'nome' => 'acarajé',
+        ]);
+
+		// Região Centro-Oeste 
+        DB::table('comidas')->insert([
+            'regiao_id' => 3,
+            'nome' => 'Arroz com Pequi',
+        ]);
+
+        DB::table('comidas')->insert([
+            'regiao_id' => 3,
+            'nome' => 'Pamonha',
+        ]);
+
+		// Região Sudeste 
+        DB::table('comidas')->insert([
+            'regiao_id' => 4,
+            'nome' => 'Pão de Queijo',
+        ]);
+
+		// Região Sul 
+        DB::table('comidas')->insert([
+            'regiao_id' => 5,
+            'nome' => 'Churrasco',
+        ]);
+
+        DB::table('comidas')->insert([
+            'regiao_id' => 5,
+            'nome' => 'Chimarrão',
+        ]);
+
     }
 }
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 set -e
+
+if [ ! -d "/var/www/html/vendor" ]; then
+  # Control will enter here if $DIRECTORY exists.
+  cd /var/www/html/
+  composer.phar install --prefer-source --no-interaction
+fi
+
+if [ ! -f "/var/www/html/.env" ]; then
+  cd /var/www/html/
+  cp .env.example .env
+fi
+
+
 apache2ctl -D FOREGROUND
 exec "$@"
